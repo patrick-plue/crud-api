c23f2ef4a2baa18575c22e2e6264a41691711d28
Patrick Pluequett <ppluequett@gmail.com>
2025-02-19 17:40:29 +0100

    add todo router

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import todoRouter from './router/todoRouter';
 
 const port = process.env.PORT;
 
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.use('/api/todo', todoRouter);
 
 app.listen(port, () =>
     console.log(`Server is listening on http://localhost:${port}`)
---
+++ router/todoRouter.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const todoRouter = Router();
+
+// todoRouter.route('/').get().post();
+
+// todoRouter.route('/:id').get().put().delete();
+
+export default todoRouter;
